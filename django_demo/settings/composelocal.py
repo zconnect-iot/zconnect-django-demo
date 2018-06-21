faf77d0e753dd9f9a43fd20eb124f9a7107b81dd
mharle <mitchell.harle@zoetrope.io>
2018-06-21 17:26:15 +0100

    Update full_client_ids

--- django_demo/settings/composelocal.py
+++ django_demo/settings/composelocal.py
@@ -28,7 +28,7 @@ ZCONNECT_SETTINGS = {
         "type": "shared",
 
         "broker-url": "vernemq",
-        "full_client_id": "zconnect-sender-{}".format(uuid.uuid4()),
+        "full_client_id": "g:abcdef:sender:599ed35d42d23c29bee30393",
         "id": "test_sender",
         "disable-tls": True,
         "port": 1883,
@@ -41,7 +41,7 @@ ZCONNECT_SETTINGS = {
         "type": "shared",
 
         "broker-url": "vernemq",
-        "full_client_id": "zconnect-listener-{}".format(uuid.uuid4()),
+        "full_client_id": "g:abcdef:listener:599ed35d42d23c29bee30394",
         "id": "test_sender",
         "disable-tls": True,
         "port": 1883,
